53881fd297bc8a24cbe215a90b24803df1edb301
Carl Wilen <dawgwaredev@gmail.com>
2016-06-27 22:51:09 -0400

    Setup file for stemlab_client.

---
+++ setup_client.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='stemlab_client',
+    version='1.0',
+    packages=['stemlab_client', 'stemlab_client.common', 'stemlab_client.sensors'],
+    url='',
+    license='Apache 2.0',
+    author='Carl Wilen',
+    author_email='dawgwaredev@gmail.com',
+    description='An example REST API client for polling sensor data and reporting back to central server.',
+    install_requires=['arrow',
+                      'requests',
+                      'futures',
+                      'path.py',
+                      'collection-json']
+)
