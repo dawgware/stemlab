5c6ab4257a23d7121ef260f04ebaf9419cce1cd5
Carl Wilen <wilenc@cisco.com>
2016-06-28 09:40:57 -0400

    Added launch method

--- stemlab_api/api.py
+++ stemlab_api/api.py
@@ -4,6 +4,7 @@ from os import path as op
 from path import Path
 from stemlab_api.models.models import Devices, Readings
 import traceback
+import argparse
 
 __version__ = 1.0
 __author__ = "dawgwaredev@gmail.com"
@@ -29,7 +30,7 @@ def init_views():
     HumidityReadingsView.register(app)
 
 
-def setup_databases():
+def setup_databases(influx_host):
 
     current_db = Path.joinpath(app.config['DATA_DIRECTORY'],'stemlab_api.db')
     if not current_db.exists():
@@ -39,18 +40,41 @@ def setup_databases():
     ##
     # Init the InfluxDB connection
     ##
-    db_client.db_connect(host='localhost', database='device_readings')
+    db_client.db_connect(host=influx_host, database='device_readings')
 
-
-if __name__ == '__main__':
-    print "INSIDE MAIN"
+def launch():
+    argparser = argparse.ArgumentParser(
+            description="""
+Sensor REST API module that provides sensor reading data storage and retrieval
+""",
+            formatter_class=argparse.RawDescriptionHelpFormatter
+            )
+    argparser.add_argument('--influx_host', help='''Ip Address or hostname for server hosting InfluxDB instance. Requires
+      a valid ip address or resolvable hostname.
+        ''',
+                           required=True)
+    argparser.add_argument('--debug', action='store_true',
+                           help='Run this API in debug mode',
+                           required=False)
+    args = argparser.parse_args()
+    influx_host = args.influx_host
+    debug = False
+    if args.debug:
+        debug = args.debug
     try:
         # app = create_app()
         # setup_databases(app)
         # init_views(app)
         create_app()
-        setup_databases()
+        setup_databases(influx_host=influx_host)
         init_views()
-        app.run(debug=True)
+        if not debug:
+            app.run(host='0.0.0.0')
+        else:
+            app.run(debug=True)
     except Exception as e:
-       print traceback.format_exc()
+        print traceback.format_exc()
+
+if __name__ == '__main__':
+    print "INSIDE MAIN"
+    launch()
